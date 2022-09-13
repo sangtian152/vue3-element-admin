334af29ce7a619f31706e495f4561b36418b9bd2
sangtian152 <jzyazn@sina.cn>
2022-09-13 11:41:20 +0800

    rebuild

---
+++ dist/static/js1/index-7ba575d5.js
@@ -0,0 +1,2 @@
+import{_ as r}from"../js2/index-ac08aa59.js";import{q as o,v as t}from"./_@vue-5d42028f.js";import"./__vendor-7feae675.js";import"./_axios-e8e747f1.js";import"./_element-plus-99884b8c.js";import"./_@popperjs-01a2ca38.js";const e={};function n(c,m){return o(),t("span",null,"\u9996\u9875")}const l=r(e,[["render",n]]);export{l as default};
+//# sourceMappingURL=index-7ba575d5.js.map
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue3-element-admin/',
+  base: './',
   build: {
     sourcemap: true,
     outDir: 'dist', //指定输出路径
