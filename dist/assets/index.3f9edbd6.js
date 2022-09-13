cb748298d92b230fe632996bf39d963a58f3e3d7
sangtian152 <jzyazn@sina.cn>
2022-09-13 09:15:43 +0800

    rebuild

---
+++ dist/assets/index.3f9edbd6.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"./index.ac968d75.js";const o={};function r(t,s){return c(),n("span",null,"\u9996\u9875")}const _=e(o,[["render",r]]);export{_ as default};
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/base/',
+  base: '/vue3-element-admin/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
