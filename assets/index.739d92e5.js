3231700a1748fc60d56ed44fa6a6139c99dda591
sangtian152 <jzyazn@sina.cn>
2022-09-13 09:11:46 +0800

    build

---
+++ assets/index.739d92e5.js
@@ -0,0 +1,1 @@
+function c(t){t=t||32;const n="ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678",a=n.length;let r="";for(let o=0;o<t;o++)r+=n.charAt(Math.floor(Math.random()*a));return r}export{c as r};
