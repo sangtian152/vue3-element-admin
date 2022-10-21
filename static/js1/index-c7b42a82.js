2548ae7d95abe2eeecaeaa0e591f13ce22d0be0a
sangtian152 <jzyazn@sina.cn>
2022-10-21 10:00:22 +0800

    增加图片质量设置

--- static/js1/index-c7b42a82.js
+++
@@ -1,2 +0,0 @@
-import{d as t,j as a,o as n,c as s,h as r,k as c}from"../js2/index-4fa15814.js";const d=t({__name:"index",setup(p){const e=r(),{params:o}=c();return a(()=>{setTimeout(()=>{e.replace({path:decodeURIComponent(o.path)})},50)}),(u,m)=>(n(),s("span"))}});export{d as default};
-//# sourceMappingURL=index-c7b42a82.js.map
